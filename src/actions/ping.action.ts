8600eaccecb010d5a317460165376139057fc38f
Stanislav Popov <popstas@gmail.com>
2021-01-03 20:46:59 +0500

    feature(pingAction): logs

--- src/actions/ping.action.ts
+++ src/actions/ping.action.ts
@@ -5,17 +5,20 @@ export const pingAction: IPingAction = {
   action: 'ping',
   run: (ping, state): any => {
     const { from } = ping;
-    console.log(ping, state);
+    console.log({ ping, state });
+    console.log('after ping')
     let connects = state?.connects || [];
     connects = connects.filter((connect) => typeof connect !== 'undefined');
     if (!connects.find((con) => con?.from == from)) {
-      console.log('PING: first ping from ' + from);
+      console.log('PING: first ping! from ' + from);
       connects.push({ from, lastConnect: nano() });
+      console.log('after connects push')
     } else {
       connects = connects.map((connect) => {
         if (connect.from == from) {
           connect.lastConnect = nano();
         }
+        console.log('PING19', connect);
         return connect;
       });
       console.log(`PING: lastConnect update`);
